b24d6db40923766f2e372b5632b96a5cc42169cd
neobii <iwilldevelopcodeforyou@gmail.com>
2018-12-07 11:08:33 -0600

    saving and retrieving values as raw draft

--- lib/DraftField.jsx
+++ lib/DraftField.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Components, registerComponent, getDynamicComponent } from 'meteor/vulcan:core';
-import { Editor, EditorState } from 'draft-js';
+import { Components, registerComponent } from 'meteor/vulcan:core';
+import { convertToRaw, convertFromRaw, Editor, EditorState } from 'draft-js';
+
+const emptyContentState = convertFromRaw({
+  entityMap: {},
+  blocks: [
+    {
+      text: '',
+      //key: 'smartFormDraft',
+      type: 'unstyled',
+      entityRanges: [],
+    },
+  ],
+});
 
 class SmartFormDraft extends Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    if(props.value || props.prefilledValue) {
+      contentState = convertFromRaw(JSON.parse(props.value));
+      this.state = {
+        editorState: EditorState.creatWithContent(contentState)
+      }
+    }
+    else {
+      this.state = {
+        editorState: EditorState.createWithContent(emptyContentState)
+      };
+    }
+    
+    this.onChange = (editorState) => {
+      this.setState({editorState});
+      this.updateValues(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+    }
     this.setEditor = (editor) => {
       this.editor = editor;
     };
@@ -16,23 +42,34 @@ class SmartFormDraft extends Component {
         this.editor.focus();
       }
     };
+    //deal with prefilled values or value that is alreayd here
   }
 
-  componentDidMount() {
-    this.focusEditor();
+  updateValues(draftRaw) {
+    const { name, updateCurrentValues } = this.props;
+    updateCurrentValues && updateCurrentValues({[name]: draftRaw});
   }
 
   render() {
+    const { name } = this.props;
     return (
       <div onClick={this.focusEditor}>
+        <h3>{name}:</h3>
         <Editor
           ref={this.setEditor}
           editorState={this.state.editorState}
           onChange={this.onChange}
+          key={`draft_${name}`}
         />
       </div>
     );
   }
 }
+
+SmartFormDraft.contextTypes = {
+  updateCurrentValues: PropTypes.func,
+  //prefilledValue: PropTypes.any,
+  //value: PropTypes.any,
+};
 
 registerComponent('SmartFormDraft', SmartFormDraft);
